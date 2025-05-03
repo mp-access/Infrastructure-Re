7320b650822469d3b3afd8fad1bdc598f8dbbc8b
Carol Alexandru <alexandru@ifi.uzh.ch>
2025-05-03 09:05:51 +0900

    also restart keycloak upon certbot renewal

--- scripts/renew_certbot.bash
+++ scripts/renew_certbot.bash
@@ -5,7 +5,9 @@
 
 docker compose down frontend
 docker compose down backend
+docker compose down keycloak
 docker compose up certbot
+docker compose up keycloak -d
 docker compose up backend -d
 docker compose up frontend -d
 
