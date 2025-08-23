eba12cc9162a4a67f99558d10fbd184b1c1c0f1e
simonhklaassen <simon.klaassen@outlook.com>
2025-08-23 14:21:03 +0200

    add quantization

--- llm-microservice/llm.py
+++ llm-microservice/llm.py
@@ -1,3 +1,6 @@
+import time
+import logging
+
 import torch
 from transformers import AutoTokenizer, AutoModel
 
@@ -6,23 +9,38 @@ tokenizer = None
 model = None
 device = None
 
+logger = logging.getLogger(__name__)
 
 def load_model_and_tokenizer():
 
     global tokenizer, model, device
     if model is None:
-        print("Loading GraphCodeBERT model and tokenizer...")
+        logger.info("Loading GraphCodeBERT model and tokenizer...")
         model_link = "microsoft/graphcodebert-base"
         tokenizer = AutoTokenizer.from_pretrained(model_link, trust_remote_code=True)
         model = AutoModel.from_pretrained(model_link, trust_remote_code=True)
 
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         model.to(device)
+
+        if (torch.device == "cpu"):
+            torch.backends.quantized.engine = "qnnpack" # "fbgemm" for x86 (Intel/AMD CPUs), "qnnpack" for ARM/Apple M-Chips
+            model_quantized = torch.quantization.quantize_dynamic(
+                model,
+                {torch.nn.Linear},
+                dtype=torch.qint8
+            )
+            model = model_quantized
+            logger.info("Model quantized successfully.")
+
         model.eval()
-        print(f"Model loaded successfully on device: {device}")
+        logger.info(f"Model loaded successfully on device: {device}")
 
 
 def calculate_code_embedding(code: str) -> list:
+
+    start_embedding_calculation = time.time()
+    logger.info(f"Starting embedding calculation: {start_embedding_calculation}")
 
     if model is None or tokenizer is None:
         raise RuntimeError("Model and tokenizer not loaded. Call load_model_and_tokenizer() first.")
@@ -42,4 +60,10 @@ def calculate_code_embedding(code: str) -> list:
     code_embedding = mean_pooled_embedding
     code_embedding = torch.nn.functional.normalize(code_embedding, p=2, dim=1)
 
-    return code_embedding.squeeze().cpu().numpy().tolist()
+    result = code_embedding.squeeze().cpu().numpy().tolist()
+
+    end_embedding_calculation = time.time()
+    logger.info(f"Ending embedding calculation: {end_embedding_calculation}")
+    logger.info(f"RES: Time required: {(end_embedding_calculation - start_embedding_calculation) * 1000}ms")
+
+    return result
